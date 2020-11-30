2eb3bece9c80a41801d0e3e20d79c00704821aa7
Babatunde Afolabi <tundeafolabi@users.noreply.github.com>
2020-11-30 09:55:35 +0100

    Add project files.

---
+++ DesignPatternInCSharp/Encapsulation/Bank.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternInCSharp.Encapsulation
+{
+    public class Bank
+    {
+        //hiding class data by declaring the variable as private
+        private double balance;
+
+        //creating public getter that returns balance
+        public double getBalance()
+        {
+            //add validation logic if needed
+            return balance;
+        }
+
+        public void setBalance(double balance)
+        {
+            //add validation to check wheter data is correct or not
+            this.balance = balance;  
+        }
+
+        //Encapsulation with properties 
+
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                if (value > 0) Console.WriteLine("value cannot be negative");
+                balance = value;
+            }
+        }
+
+    }
+}
---
+++ DesignPatternInCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using DesignPatternInCSharp.Encapsulation;
+using System;
+
+namespace DesignPatternInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test To show encapsulation
+            Bank access = new Bank();
+            access.setBalance(500);
+            Console.WriteLine(access.getBalance());
+
+            //Encapsulation using properties in c#
+            access.Balance = -100;
+            Console.WriteLine(access.Balance);
+            access.Balance = 50;
+            Console.WriteLine(access.Balance);
+        }
+    }
+}
