e88fab98cb183ad84a3f08e9f67b020ce1df335b
Babatunde Afolabi <tundeafolabi@users.noreply.github.com>
2020-11-30 13:32:13 +0100

    CompleteSingleInheritance

---
+++ DesignPatternInCSharp/Inheritance/Branch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternInCSharp.Inheritance
+{
+    /// <summary>
+    /// Inheritance is the principle that helps to achieve resuability and changeability of our code.
+    /// It helps to use the exisiting features of an object as well as override the existing functions also.
+    /// It is basically transfering properties and behaviours of an existing class
+    /// </summary>
+    class Branch
+    {
+        private int branchCode;
+        private string branchName, branchAddress;
+
+        public void GetBranchData()
+        {
+            Console.WriteLine("Enter branch details:");
+            Console.WriteLine("Enter branch code");
+            branchCode = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter branch name");
+            branchName = Console.ReadLine();
+            Console.WriteLine("Enter branch address");
+            branchAddress = Console.ReadLine();
+        }
+
+        public void DisplayBranchData()
+        {
+            Console.WriteLine("Branch code is:" + branchCode);
+            Console.WriteLine("Branch name is:" +  branchName);
+            Console.WriteLine("Branch address is:" + branchAddress);
+        }
+    }
+
+    class Employee : Branch
+    {
+        private int employeeId, employeeAge;
+        private string employeeName, employeeAddress;
+
+        public void GetEmployeeData()
+        {
+            Console.WriteLine("Enter employeee details:");
+            Console.WriteLine("Enter employee ID");
+            employeeId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter employee Age");
+            employeeAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter employee Name");
+            employeeName = Console.ReadLine();
+            Console.WriteLine("Enter employee address");
+            employeeAddress = Console.ReadLine();
+        }
+
+        public void DisplayEmployeeData()
+        {
+            Console.WriteLine("Employee ID is :" + employeeId);
+            Console.WriteLine("Employee name is :" + employeeName);
+            Console.WriteLine("Employee Address is : " + employeeAddress);
+            Console.WriteLine("Employee age is :" + employeeAge);
+
+        }
+
+    }
+}
--- DesignPatternInCSharp/Program.cs
+++ DesignPatternInCSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DesignPatternInCSharp.Abstraction;
 using DesignPatternInCSharp.Encapsulation;
+using DesignPatternInCSharp.Inheritance;
 using System;
 
 namespace DesignPatternInCSharp
@@ -9,26 +10,35 @@ namespace DesignPatternInCSharp
         static void Main(string[] args)
         {
             //Test To show encapsulation
-            Bank access = new Bank();
-            access.setBalance(500);
-            Console.WriteLine(access.getBalance());
+            //Bank access = new Bank();
+            //access.setBalance(500);
+            //Console.WriteLine(access.getBalance());
 
             //Encapsulation using properties in c#
-            access.Balance = -100;
-            Console.WriteLine(access.Balance);
-            access.Balance = 50;
-            Console.WriteLine(access.Balance);
+            //access.Balance = -100;
+            //Console.WriteLine(access.Balance);
+            //access.Balance = 50;
+            //Console.WriteLine(access.Balance);
 
 
             //Test to show Abstraction
-            Car carObject = new Car();
-            carObject.CarName = "toyota";
-            carObject.CarColour = "icewine";
+            //Car carObject = new Car();
+            //carObject.CarName = "toyota";
+            //carObject.CarColour = "icewine";
 
-            carObject.Brakes();
-            carObject.Gear();
-            carObject.Steering();
+            //carObject.Brakes();
+            //carObject.Gear();
+            //carObject.Steering();
 
+
+            //Test to show Inheritance 
+            //Employee employee = new Employee();
+            //employee.GetBranchData();
+            //employee.GetEmployeeData();
+            //employee.DisplayBranchData();
+            //employee.DisplayEmployeeData();
+            //Console.WriteLine("Press any key to exit");
+            //Console.ReadKey();
 
         }
     }
